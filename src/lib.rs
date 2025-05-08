1bbe0042ba08ac2a9f7b5f92fa672072acc5e042
Emre Iyican <git@iyican.me>
2025-05-08 08:04:39 +0300

    Add ReadBuffer, WriteBuffer traits

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,52 @@ mod big_read;
 mod big_write;
 mod little_read;
 mod little_write;
+
+pub trait ReadBuffer {
+    fn skip(&mut self, amount: usize);
+
+    fn get_u8(&mut self) -> u8;
+
+    fn get_u16(&mut self) -> u16;
+
+    fn get_u24(&mut self) -> u32;
+
+    fn get_u32(&mut self) -> u32;
+
+    fn get_u64(&mut self) -> u64;
+
+    fn get_u128(&mut self) -> u128;
+
+    fn get_f32(&mut self) -> f32;
+
+    fn get_f64(&mut self) -> f64;
+
+    fn get_slice(&mut self, length: usize) -> Vec<u8>;
+
+    fn to_vec(&self) -> Vec<u8>;
+}
+
+pub trait WriteBuffer {
+    #[must_use]
+    fn into_vec(self) -> Vec<u8>;
+
+    fn skip(&mut self, amount: usize);
+
+    fn put_u8(&mut self, value: u8);
+
+    fn put_u16(&mut self, value: u16);
+
+    fn put_u24(&mut self, value: u32);
+
+    fn put_u32(&mut self, value: u32);
+
+    fn put_u64(&mut self, value: u64);
+
+    fn put_u128(&mut self, value: u128);
+
+    fn put_f32(&mut self, value: f32);
+
+    fn put_f64(&mut self, value: f64);
+
+    fn put_slice(&mut self, slice: &[u8]);
+}
--- src/little_write.rs
+++ src/little_write.rs
@@ -1,3 +1,4 @@
+use crate::WriteBuffer;
 use bytes::BufMut;
 
 #[derive(Default, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
@@ -10,96 +11,98 @@ impl LittleEndianWriteBuffer {
     pub const fn new() -> Self {
         Self { buffer: Vec::new() }
     }
+}
 
+impl WriteBuffer for LittleEndianWriteBuffer {
     #[must_use]
-    pub fn into_vec(self) -> Vec<u8> {
+    fn into_vec(self) -> Vec<u8> {
         self.buffer
     }
 
-    pub fn skip(&mut self, amount: usize) {
+    fn skip(&mut self, amount: usize) {
         let new_size = self.buffer.len() + amount;
         self.buffer.resize(new_size, 0x00);
     }
 
-    pub fn put_u8(&mut self, value: u8) {
+    fn put_u8(&mut self, value: u8) {
         self.buffer.put_u8(value);
     }
 
     #[cfg(target_endian = "little")]
-    pub fn put_u16(&mut self, value: u16) {
+    fn put_u16(&mut self, value: u16) {
         self.buffer.put_u16(value);
     }
 
     #[cfg(target_endian = "big")]
-    pub fn put_u16(&mut self, value: u16) {
+    fn put_u16(&mut self, value: u16) {
         self.buffer.put_u16_le(value);
     }
 
     #[cfg(target_endian = "little")]
-    pub fn put_u24(&mut self, value: u32) {
+    fn put_u24(&mut self, value: u32) {
         self.buffer.put_u8((value & 0x00_00_00_FF) as u8);
         self.buffer.put_u8(((value & 0x00_00_FF_00) >> 8) as u8);
         self.buffer.put_u8(((value & 0x00_FF_00_00) >> 16) as u8);
     }
 
     #[cfg(target_endian = "big")]
-    pub fn get_u24(&mut self, value: u32) {
+    fn get_u24(&mut self, value: u32) {
         self.buffer.put_u8(((value & 0x00_FF_00_00) >> 16) as u8);
         self.buffer.put_u8(((value & 0x00_00_FF_00) >> 8) as u8);
         self.buffer.put_u8((value & 0x00_00_00_FF) as u8);
     }
 
     #[cfg(target_endian = "little")]
-    pub fn put_u32(&mut self, value: u32) {
+    fn put_u32(&mut self, value: u32) {
         self.buffer.put_u32(value);
     }
 
     #[cfg(target_endian = "big")]
-    pub fn put_u32(&mut self, value: u32) {
+    fn put_u32(&mut self, value: u32) {
         self.buffer.put_u32_le(value);
     }
 
     #[cfg(target_endian = "little")]
-    pub fn put_u64(&mut self, value: u64) {
+    fn put_u64(&mut self, value: u64) {
         self.buffer.put_u64(value);
     }
 
     #[cfg(target_endian = "big")]
-    pub fn put_u64(&mut self, value: u64) {
+    fn put_u64(&mut self, value: u64) {
         self.buffer.put_u64_le(value);
     }
 
     #[cfg(target_endian = "little")]
-    pub fn put_u128(&mut self, value: u128) {
+    fn put_u128(&mut self, value: u128) {
         self.buffer.put_u128(value);
     }
 
     #[cfg(target_endian = "big")]
-    pub fn put_u128(&mut self, value: u128) {
+    fn put_u128(&mut self, value: u128) {
         self.buffer.put_u128_le(value);
     }
 
     #[cfg(target_endian = "little")]
-    pub fn put_f32(&mut self, value: f32) {
-        self.buffer.put_f32(value)
+    fn put_f32(&mut self, value: f32) {
+        self.buffer.put_f32(value);
     }
 
     #[cfg(target_endian = "big")]
-    pub fn put_f32(&mut self, value: f32) {
+    fn put_f32(&mut self, value: f32) {
         self.buffer.put_f32_le(value);
     }
 
     #[cfg(target_endian = "little")]
-    pub fn put_f64(&mut self, value: f64) {
+    fn put_f64(&mut self, value: f64) {
         self.buffer.put_f64(value);
     }
 
     #[cfg(target_endian = "big")]
-    pub fn put_f64(&mut self, value: f64) {
+    fn put_f64(&mut self, value: f64) {
         self.buffer.put_f64_le(value);
     }
 
-    pub fn put_slice(&mut self, slice: &[u8]) {
+    fn put_slice(&mut self, slice: &[u8]) {
         self.buffer.extend(slice);
     }
 }
